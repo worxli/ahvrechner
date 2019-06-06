ea593e6f6aef9fb4a29aa892768cf6874c3be4fc
Lukas Bischofberger <me@worx.li>
2019-06-06 21:46:22 +0200

    Update angular

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 
-import {
-  MatInputModule,
-  MatSliderModule,
-  MatFormFieldModule,
-  MatCheckboxModule,
-  MatCardModule,
-  MatRadioModule
-} from '@angular/material';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { PlatformModule } from '@angular/cdk/platform';
 import { ObserversModule } from '@angular/cdk/observers';
